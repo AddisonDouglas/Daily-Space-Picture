5d2aa9ae568b5a50d55215e512dce8693b5eed63
Addison Douglas <addisondouglas850@gmail.com>
2024-01-16 11:26:41 -0800

    Swapped to Axios for API fetching

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import './App.css';
+import Axios from "axios";
+import {useEffect, useState} from "react";
 
-fetch("https://api.nasa.gov/planetary/apod?api_key=EZ4gnkdhhUyJs7cfkAZ56SGiY1H2wCgPMbSSQ5qO")
-  .then((res) => res.json())
-  .then((data) =>{
-    console.log(data);
 
-  });
+
+
+
+
 
   // test commit 2
 
 function App() {
+  const [SpacePicture, setSpacePicture] = useState("");
+
+  useEffect(() => {
+  Axios.get("https://api.nasa.gov/planetary/apod?api_key=EZ4gnkdhhUyJs7cfkAZ56SGiY1H2wCgPMbSSQ5qO").then((res) =>{
+    setSpacePicture(res.data.hdurl);
+  });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-      <h1>HEWO</h1>
+      <h1>{SpacePicture}</h1>
       </header>
     </div>
   );
