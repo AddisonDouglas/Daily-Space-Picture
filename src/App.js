676c9493d0b82e342341776a0ffa1bd8c5274635
Addison Douglas <addisondouglas850@gmail.com>
2024-01-17 17:58:16 -0800

    Changed from showing url to showing image and resizing based on browser size.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
 
   useEffect(() => {
   Axios.get("https://api.nasa.gov/planetary/apod?api_key=EZ4gnkdhhUyJs7cfkAZ56SGiY1H2wCgPMbSSQ5qO").then((res) =>{
+    console.log(res.data);
     setSpacePicture(res.data.hdurl);
   });
   }, []);
@@ -15,7 +16,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <h1>{SpacePicture}</h1>
+        <img src ={SpacePicture} className="photo"></img>
+      <h1></h1>
       </header>
     </div>
   );
