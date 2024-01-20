5c295ef79bfef5df6f55bcfec0355ec9c74d1e4c
Addison Douglas <addisondouglas850@gmail.com>
2024-01-20 11:06:07 -0800

    added photo explanation

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import {useEffect, useState} from "react";
 function App() {
   const [SpacePicture, setSpacePicture] = useState("");
   const [SpaceDate, setSpaceDate] = useState("");
+  const [explanation, setExplantion] = useState("");
 
   useEffect(() => {
   Axios.get("https://api.nasa.gov/planetary/apod?api_key=EZ4gnkdhhUyJs7cfkAZ56SGiY1H2wCgPMbSSQ5qO").then((res) =>{
     console.log(res.data);
     setSpaceDate(res.data.date);
+    setExplantion(res.data.explanation);
     setSpacePicture(res.data.hdurl);
   });
   }, []);
@@ -19,7 +21,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src ={SpacePicture} className="photo"></img>
-      <h1>{SpaceDate}</h1>
+      <h8>{SpaceDate}</h8>
+      <h6>{explanation}</h6>
+      
+      
       </header>
     </div>
   );
