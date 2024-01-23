03fa0a05df4e134ba79a71ca1a9e652548568538
Addison Douglas <addisondouglas850@gmail.com>
2024-01-23 13:45:43 -0800

    added title, and changed formatting

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
   const [SpacePicture, setSpacePicture] = useState("");
   const [SpaceDate, setSpaceDate] = useState("");
   const [explanation, setExplantion] = useState("");
+  const [Title, setTitle] = useState("");
+
 
   useEffect(() => {
   Axios.get("https://api.nasa.gov/planetary/apod?api_key=EZ4gnkdhhUyJs7cfkAZ56SGiY1H2wCgPMbSSQ5qO").then((res) =>{
@@ -14,18 +16,19 @@ function App() {
     setSpaceDate(res.data.date);
     setExplantion(res.data.explanation);
     setSpacePicture(res.data.hdurl);
+    setTitle(res.data.title);
+
   });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src ={SpacePicture} className="photo"></img>
+        <h1>{Title}</h1>
+      </header>
+      <img src ={SpacePicture} className="photo"></img>
       <h8>{SpaceDate}</h8>
       <h6>{explanation}</h6>
-      
-      
-      </header>
     </div>
   );
 }
